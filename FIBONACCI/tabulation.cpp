08cbd9482fb8ceb6cf60f7e2400d6c6ab028719c
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-09-21 18:34:02 +0530

    Tabulation Method

---
+++ FIBONACCI/tabulation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int CalculateFibN(int N)
+{
+
+    if (N <= 1)
+        return N;
+
+    vector<int> dp(N + 1);
+    dp[0]=0;
+    dp[1]=1;
+
+    for (int i = 2; i <= N; i++){
+        dp[i]=dp[i-1]+dp[i-2];
+    }
+
+    return dp[N];
+}
+
